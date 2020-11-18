0cfaefcdb09ff10c2b50c5e093195261f25bc3dc
redfish1 <scottzhou@hotmail.com>
2020-11-17 18:14:07 -0600

    change1 done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello World
+          Hello World2
         </p>
         <a
           className="App-link"
